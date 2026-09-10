be4b67e87c92a94717aa0d64e718763564b0f400
Ashmit Jain <ashmitjain54@gmail.com>
2026-09-10 18:03:28 +0530

    authroutes file has been created

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express =require("express");
 
-const app=express();
+const app=express(); //server ka instance create kiya
 
-app.use(express.json());
+app.use(express.json()); //middleware use kiya express.json jisse req.body mein data read kr paaye
+/*  require all the routes here */
+const authRouter = require("./routes/auth.routes");
 
+
+/* using all the routes here */
+app.use("/api/auth",authRouter); /* auth related jitni bhi api hoti hai usko access krne ke liye /api/auth 
+prefix lgana jruri hai */
 
 module.exports=app;
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+const {Router} = require('express'); /* kyuki hum api app.js mein nhi bna rhe hai uske liye alg se
+ routes folder bnaya hai toh hume firse express require krna hota hai usme bhi route require krna
+  hota jo hum directly aise kr skte hai */
+
+const authRouter = Router();
+
+
+module.exports=authRouter;
