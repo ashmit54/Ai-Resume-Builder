ce1d6afa99185bbf7223b15dc6886abce0063976
Ashmit Jain <ashmitjain54@gmail.com>
2026-09-22 12:54:55 +0530

    Created protected component

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+ 
+
+export async function register({username, email,password}) {
+
+    try {
+        const response = await axios.post("http://localhost:3000/api/auth/register", {
+            username, email, password
+        }, {
+            withCredentials:true // agr isko true nhi krenge to server cookies mein data nhi send kr paayega usse access nhi kr payega
+        })
+
+
+        return response.data;
+    } catch (err) {
+
+        console.log(err);
+        
+    }
+    
+} 
+
+
+export async function login({email,password}) {
+    try{
+        const response=await axios.post("http://localhost:3000/api/auth/login",{
+            email,password
+        },{
+            withCredentials:true
+        })
+
+        return response.data;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+export async function logout() {
+    try {
+        const response=await axios.get("http://localhost:3000/api/auth/logout",{
+            withCredentials:true
+        })
+        return response.data;
+    } catch (err) {
+        
+    }
+    
+}
+
+export async function getMe(){
+    try {
+        const response =await axios.get("http://localhost:3000/api/auth/get-me",{
+            withCredentials:true
+        })
+        return response.data
+
+
+    } catch(err){
+        console.log(err);
+    }
+}
+
